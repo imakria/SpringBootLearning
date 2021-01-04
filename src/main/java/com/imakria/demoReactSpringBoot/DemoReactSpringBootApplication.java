577ab9368b7d0cea8848be045267ccbdcdfe6d6a
imakria <imakria@gmail.com>
2021-01-04 08:54:05 +0100

    first commit

---
+++ src/main/java/com/imakria/demoReactSpringBoot/DemoReactSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.imakria.demoReactSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoReactSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoReactSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imakria/demoReactSpringBoot/DemoReactSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imakria.demoReactSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReactSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
